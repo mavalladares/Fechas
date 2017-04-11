6603c0739e3bb337c4620b7b8f7a5295b551b98b
mavalladares <moisesiti@gmail.com>
2017-04-11 10:42:41 -0600

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,7 +1,8 @@
 while True:
    print("Options:")
-   print("Enter 'add' to add two numbers")
-   print("Enter 'subtract' to subtract two numbers")
+   print("Presione "1" para calculos referente a nuevo impuestos y CB")
+   print("Presione '2' para poder consultar datos del cliente por DUI")
+   print("Presione '3' para generar contrato")
    print("Enter 'multiply' to multiply two numbers")
    print("Enter 'divide' to divide two numbers")
    print("Enter 'quit' to end the program")
@@ -9,12 +10,15 @@ while True:
 
    if user_input == "quit":
       break
-   elif user_input == "add":
-      elif user_input == "add":
-  num1 = float(input("Enter a number: "))
-  num2 = float(input("Enter another number: "))
-   elif user_input == "subtract":
-       elif user_input == "add":
+   elif user_input == "1":
+  print("Options:")
+   print("Presione "1" para calculos referente a nuevo impuestos y CB")
+   print("Presione '2' para poder consultar datos del cliente por DUI")
+   print("Presione '3' para generar contrato")
+   print("Enter 'multiply' to multiply two numbers")
+   print("Enter 'divide' to divide two numbers")
+   print("Enter 'quit' to end the program")
+   user_input = input(": ")
   num1 = float(input("Enter a number: "))
   num2 = float(input("Enter another number: "))
    elif user_input == "multiply":
